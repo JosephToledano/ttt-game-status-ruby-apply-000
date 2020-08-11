b744d8ce7fca5878c85c1f22458f454083fa563e
Joseph Toledano <joseph.a.toledano@gmail.com>
2020-08-11 02:40:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,10 +30,10 @@ def won?(board)
 
     # Checking to see if there's a winning combination and representing the winning indices
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
+          return winning_combination
+        elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+          return winning_combination
 
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-       return win_combination
      end
    }
    return false
