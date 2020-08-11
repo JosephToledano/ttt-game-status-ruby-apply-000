a338a02cc9b9cb6cd0d6341044721cc7c7481962
Joseph Toledano <joseph.a.toledano@gmail.com>
2020-08-11 02:28:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,37 +39,6 @@ def won?(board)
    return false
  end
 
- def won?(board)
-   WIN_COMBINATIONS.each {|win_combo|
-     index_0 = win_combo[0]
-     index_1 = win_combo[1]
-     index_2 = win_combo[2]
-
-     position_1 = board[index_0]
-     position_2 = board[index_1]
-     position_3 = board[index_2]
-
-     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-       return win_combo
-     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-       return win_combo
-     end
-   }
-   return false
- end
-
-
-
-
-
-
-
-
-
-
-
-
-
  #Checking to see if every space on the board is taken
  def full?(board)
    board.all? {|index| index = "X" || index = "O"}
