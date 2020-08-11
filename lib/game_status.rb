34024c278eb0fbc80006e53660fc631ca02fa794
Joseph Toledano <joseph.a.toledano@gmail.com>
2020-08-11 02:51:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,9 +39,8 @@ end
  def draw?(board)
   if !won?(board) && full?(board)
     return true
-  elsif !won?(board) && !full?(board) || !won?(board)
-    return false
-  end
+  else
+    return false 
 end
 
  #Checking to see if the game is over
