9ecdac9527923930e167058755fdb87003fec988
Joseph Toledano <joseph.a.toledano@gmail.com>
2020-08-11 03:17:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,43 +5,50 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-[0,1,2], # top_row
-[3,4,5], # middle_row
-[6,7,8], # bottom_row
-[0,3,6], # left_column
-[1,4,7], # center_column
-[2,5,8], # right_column
-[0,4,8], # left_diagonal
-[6,4,2] # right_diagonal
+  [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
 ]
 
 #Won? Method
 #Checking to see if somebody won and displaying the board
 def won?(board)
- win_index_1 = win_combination[0]
- win_index_2 = win_combination[1]
- win_index_3 = win_combination[2]
- position_1 = board[win_index_1] # value of board at win_index_1
- position_2 = board[win_index_2] # value of board at win_index_2
- position_3 = board[win_index_3] # value of board at win_index_3
- position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
 end
 
 
  #Checking to see if every space on the board is taken
  def full?(board)
-   board.all? {|i| i == "X" || i == "O"}
+  board.all? {|index| index == "X" || index == "O"}
 end
 
  #Checking to see if there's a draw
  def draw?(board)
-   if !won?(board) && full?(board)
-     return true
-   elsif !won?(board) && !full?(board)
-     return false
-   else won?(board)
-     return false
-   end
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
 end
 
  #Checking to see if the game is over
