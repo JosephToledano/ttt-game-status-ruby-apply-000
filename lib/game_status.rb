7d69f42bbe6f9baa101bf1491dc693c88c979989
Joseph Toledano <joseph.a.toledano@gmail.com>
2020-08-11 02:36:56 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,11 +8,11 @@ WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5], # Middle row
   [6,7,8], # Bottom row
-  [0,4,8], # Diagonal Left-to-Right
-  [2,4,6], # Diagonal Right-to_Left
   [0,3,6], # Left column
   [1,4,7], # Middle columns
   [2,5,8]  # Right Column
+  [0,4,8], # Diagonal Left-to-Right
+  [2,4,6], # Diagonal Right-to_Left
 ]
 
 #Won? Method
