b5c384d3c0d4e55bd02023123051d8aa8a467307
Joseph Toledano <joseph.a.toledano@gmail.com>
2020-08-11 03:10:27 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,28 +5,30 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,3,6],
-[1,4,7],
-[2,5,8],
-[0,4,8],
-[2,4,6]
+[0,1,2], # top_row
+[3,4,5], # middle_row
+[6,7,8], # bottom_row
+[0,3,6], # left_column
+[1,4,7], # center_column
+[2,5,8], # right_column
+[0,4,8], # left_diagonal
+[6,4,2] # right_diagonal
 ]
 
 #Won? Method
 #Checking to see if somebody won and displaying the board
 def won?(board)
-  WIN_COMBINATIONS.each do |winner_combo|
-      if board[winner_combo[0]] == board[winner_combo[1]] &&
-         board[winner_combo[1]] == board[winner_combo[2]] &&
-         position_taken?(board, winner_combo[0])
-         return true
-         return winner_combo
-      end
-    end
-  end
+  WIN_COMBINATIONS.each do |win_combination|
+   win_index_1 = win_combination[0]
+   win_index_2 = win_combination[1]
+   win_index_3 = win_combination[2]
+   position_1 = board[win_index_1] # value of board at win_index_1
+   position_2 = board[win_index_2] # value of board at win_index_2
+   position_3 = board[win_index_3] # value of board at win_index_3
+   
+   position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+ end
+end
 
  #Checking to see if every space on the board is taken
  def full?(board)
@@ -47,7 +49,7 @@ end
    if full?(board) || !(won?(board)) || draw?(board)
     return true
   else
-    return false 
+    return false
   end
 end
 
