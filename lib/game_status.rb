7c78f5162e95e27078ad321add4cfa991f5cf4ee
Joseph Toledano <joseph.a.toledano@gmail.com>
2020-08-11 03:21:45 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,73 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+    [0, 1, 2], #Top row
+    [3, 4, 5], #Middle row
+    [6, 7, 8], #Bottom row
+    [0, 3, 6], #Left column
+    [1, 4, 7], #Middle column
+    [2, 5, 8], #Right column
+    [0, 4, 8], #Left to right diagonal
+    [6, 4, 2]  #Right to left diagonal
+]
+
+#Won? Method
+#Checking to see if somebody won and displaying the board
+def won?(board)
+  WIN_COMBINATIONS.each {|winning_combo|
+    index0 = winning_combo[0]
+    index1 = winning_combo[1]
+    index2 = winning_combo[2]
+
+    position_1 = board[index0]
+    position_2 = board[index1]
+    position_3 = board[index2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return winning_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return winning_combo
+    end
+  }
+  return false
+end
+
+
+ #Checking to see if every space on the board is taken
+ def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
+
+ #Checking to see if there's a draw
+ def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+ #Checking to see if the game is over
+ def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+ #Showing the "X" or "O" that has won the game
+ def winner(board)
+   iindex = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
