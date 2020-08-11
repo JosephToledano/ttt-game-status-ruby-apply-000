54f70e3254e808707599a0d2b30a3914d18e0705
Joseph Toledano <joseph.a.toledano@gmail.com>
2020-08-11 02:54:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,14 +5,14 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5], # Middle row
-  [6,7,8], # Bottom row
-  [0,3,6], # Left column
-  [1,4,7], # Middle columns
-  [2,5,8],  # Right Column
-  [0,4,8], # Diagonal Left-to-Right
-  [2,4,6] # Diagonal Right-to_Left
+  [0,1,2], #Top row
+ [3,4,5], #Middle row
+ [6,7,8], #Bottom row
+ [0,3,6], #Left column
+ [1,4,7], #Middle column
+ [2,5,8], #Right column
+ [0,4,8], #Left start diagonal
+ [2,4,6]  #Right start diagonal
 ]
 
 #Won? Method
